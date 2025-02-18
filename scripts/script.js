5aa74112229450d4af95be78e9d0941f7f43efe1
Emilia Holmqvist <emiliaholmqvist@live.se>
2025-02-18 10:08:24 +0100

    fixat search

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,22 @@
 import { renderRandomTrailers } from "./modules/gu.js";
 import { topMoviesCard } from "./modules/gu.js";
-
-
+import { searchBtnFunc } from "./components/search.js";
+/* import { searchPage } from "./components/search.js"; */
+/* import { getMovieArray } from "./components/search.js";
+ *//* import { saveSearchValue } from "./components/search.js";
+ *//* import { saveSearchValue} from "./components/search.js"; */
+/* import { fetchMovieLibrary } from "./modules/api.js"; */
+import { renderSearchMovie } from "./components/search.js";
+/* import { saveSearch } from "./components/search.js";
+ */
 
 if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
     console.log('index.html');
     renderRandomTrailers();
     topMoviesCard();
+    searchBtnFunc();
+    /* saveSearchValue(); */
     
-
 } else if (window.location.pathname === '/favorites.html') {
     console.log('favorites.html');
 
@@ -17,5 +25,6 @@ if (window.location.pathname === '/' || window.location.pathname === '/index.htm
 
 } else if (window.location.pathname === '/search.html') {
     console.log('search.html');
-
+    /* saveSearch(); */
+    renderSearchMovie();
 }
